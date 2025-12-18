c20abf873e6748a803e40a8570144d60f4495a35
vasilicbosko <vasilic.bosko@jjzmaj.edu.rs>
2025-12-18 03:51:08 +0100

    Feat: add ui for all profile tabs

--- web/src/app/components/profile/profile.ts
+++ web/src/app/components/profile/profile.ts
@@ -1,22 +1,42 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { bootstrapPersonCircle, bootstrapPencilFill } from '@ng-icons/bootstrap-icons';
+import { bootstrapPersonCircle, bootstrapPencilFill, bootstrapClockFill } from '@ng-icons/bootstrap-icons';
 import { ProfileCard} from './profile-card/profile-card';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   imports: [
     RouterLink,
     NgIcon,
-    ProfileCard
+    ProfileCard,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
-  viewProviders: [provideIcons({ bootstrapPersonCircle,bootstrapPencilFill })]
+  viewProviders: [provideIcons({ bootstrapPersonCircle,bootstrapPencilFill, bootstrapClockFill })]
 })
 export class ProfileComponent {
   public selectedTab: string = 'Personal Information';
+
+  showPassword = false;
+  showConfirmPassword = false;
+  showOldPassword = false;
+
+  toggleOldPassword() {
+    this.showOldPassword = !this.showOldPassword;
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPassword() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
   constructor() {
   }
 
