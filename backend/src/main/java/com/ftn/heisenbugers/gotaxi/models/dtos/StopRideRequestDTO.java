e92b91a41e98a2af35485155e3dd9df7ac83cbc1
Viktoriia <115942207+flokxxy@users.noreply.github.com>
2025-12-28 21:34:01 +0100

    Feat: add RideActionsController

---
+++ backend/src/main/java/com/ftn/heisenbugers/gotaxi/models/dtos/StopRideRequestDTO.java
@@ -0,0 +1,12 @@
+package com.ftn.heisenbugers.gotaxi.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StopRideRequestDTO {
+    private String note;
+}
