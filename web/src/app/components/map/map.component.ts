e0c7c0f585f89ce87c63d75a2ab79d9c9fa8725a
Aleksa Dejanović <dejanovicaleksa234@gmail.com>
2026-01-10 03:37:53 +0100

    Feat: add snapping to road on map

--- web/src/app/components/map/map.component.ts
+++ web/src/app/components/map/map.component.ts
@@ -1,10 +1,12 @@
 // map.component.ts
 import { Component, Input, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet-routing-machine';
 
 export interface MapPin {
   lat: number;
   lng: number;
+  snapToRoad?: boolean;
   popup?: string | HTMLElement;
   iconUrl?: string;
   iconColor?: string;
@@ -30,23 +32,27 @@ export class MapComponent implements AfterViewInit, OnChanges {
     L.control.zoom({ position: 'bottomright' }).addTo(this.map);
     L.control.scale().addTo(this.map);
 
-    this.renderPins();
+     this.snapPinsToRoad().then((pins) => {
+            this.renderPins(pins);
+        });
   }
 
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['pins'] && this.map) {
-      this.renderPins();
+        this.snapPinsToRoad().then((pins) => {
+            this.renderPins(pins);
+        });
     }
   }
 
-  private renderPins() {
+  private renderPins(pinArgs: MapPin[]) {
     // remove old markers
     this.markers.forEach(m => m.remove());
     this.markers = [];
 
     // add new markers
-    this.pins.forEach(pin => {
+    pinArgs.forEach(pin => {
         let icon: L.Icon | L.DivIcon | undefined = undefined;
         let marker: L.Marker;
 
@@ -58,13 +64,47 @@ export class MapComponent implements AfterViewInit, OnChanges {
             });
 
             marker = L.marker([pin.lat, pin.lng], { icon })
-        } else{
+        } else {
             marker = L.marker([pin.lat, pin.lng]);
         }
 
         marker.addTo(this.map)
             .bindPopup(pin.popup || '');
         this.markers.push(marker);
+        
+        });
+    }
+    private snapPinsToRoad(): Promise<MapPin[]> {
+        const snapPromises = this.pins.map(async pin => {
+            if (pin.snapToRoad) {
+                const snappedLatLng = await this.snapToRoad(pin.lat, pin.lng);
+                pin.lat = snappedLatLng.lat;
+                pin.lng = snappedLatLng.lng;
+            }
+            return pin;
+            
+        });
+
+        return Promise.all(snapPromises);
+    }
+
+    snapToRoad(lat: number, lng: number): Promise<L.LatLng> {
+        return new Promise((resolve, reject) => {
+            const url = `https://router.project-osrm.org/nearest/v1/driving/${lng},${lat}?number=1`;
+
+            fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                if (data.code === 'Ok' && data.waypoints.length) {
+                const snapped = data.waypoints[0].location; // [lng, lat]
+                resolve(L.latLng(snapped[1], snapped[0]));
+                } else {
+                reject('No road found nearby');
+                }
+            })
+            .catch(err => reject(err));
         });
     }
 }
+
+
