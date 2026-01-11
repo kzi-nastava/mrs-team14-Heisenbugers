2f6586dd443d4e131d6d3fcbea390d76058d4951
Aleksa Dejanović <dejanovicaleksa234@gmail.com>
2026-01-11 02:39:08 +0100

    Feat: add rating from during-ride view

--- web/src/app/during-ride/during-ride.component.ts
+++ web/src/app/during-ride/during-ride.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { carSelectedIcon, MapComponent } from '../components/map/map.component';
 import { NgIcon, provideIcons } from "@ng-icons/core";
-import { bootstrapExclamationCircleFill, bootstrapChatDots, bootstrapFeather } from '@ng-icons/bootstrap-icons';
+import { bootstrapExclamationCircleFill, bootstrapChatDots, bootstrapFeather, bootstrapStar, bootstrapStarFill } from '@ng-icons/bootstrap-icons';
 import { MapPin } from '../components/map/map.component';
 import { FormsModule, NgForm } from '@angular/forms';
 
@@ -10,15 +10,19 @@ import { FormsModule, NgForm } from '@angular/forms';
   selector: 'app-during-ride',
   imports: [MapComponent, NgIcon, FormsModule],
   templateUrl: './during-ride.component.html',
-  viewProviders: [provideIcons({ bootstrapExclamationCircleFill, bootstrapChatDots, bootstrapFeather })]
+  viewProviders: [provideIcons({ bootstrapExclamationCircleFill, bootstrapChatDots, bootstrapFeather, bootstrapStar, bootstrapStarFill })]
 
 })
 export class DuringRide {
+  
+  
   @ViewChild('noteFocus') noteFocus!: ElementRef<HTMLInputElement>;
-  @ViewChild('noteTitle') noteTitle!: ElementRef<HTMLInputElement>;
-
   
   NotesIsOpen: boolean = false;
+  rateIsOpen: boolean = true;
+  driverRate = 0;
+  vehicleRate = 0;
+
   location: MapPin = { lat: 45.2396, lng: 19.8227, popup: 'You are here', iconUrl: carSelectedIcon };
   passengers = [
     { name: 'Alice Alisic', avatar: 'https://i.pravatar.cc/150?img=1' },
@@ -36,10 +40,41 @@ export class DuringRide {
     this.NotesIsOpen = true;
   }
 
+  closeRateModal() {
+  this.rateIsOpen = false;
+  }
+
+  openRateModal() {
+    this.driverRate = this.vehicleRate = 0;
+    this.rateIsOpen = true;
+  }
+
   submitForm(form: NgForm) {
     if(form.valid)
-    console.log(form.value);
+      console.log(form.value);
     this.closeModal();
+  }
+
+  submitRateForm(form: NgForm) {
+    if(form.valid && this.driverRate > 0 && this.vehicleRate > 0)
+      console.log(form.value, this.driverRate, this.vehicleRate);
+    this.closeRateModal();
+  }
+
+  getDriverRateArray(): boolean[] {
+    return Array(this.driverRate).fill(true).concat(Array(5 - this.driverRate).fill(false));
+  }
+
+  setDriverRate(value: number) {
+    this.driverRate = value;
+  }
+
+  getVehicleRateArray(): boolean[] {
+    return Array(this.vehicleRate).fill(true).concat(Array(5 - this.vehicleRate).fill(false));
+  }
+
+  setVehicleRate(value: number) {
+    this.vehicleRate = value;
   }
 
 }
