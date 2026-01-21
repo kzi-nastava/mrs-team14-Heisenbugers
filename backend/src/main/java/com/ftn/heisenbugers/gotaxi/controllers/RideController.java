1487a9ccc67959f5cd186702fd45d5b7a693b274
Aleksa Dejanović <dejanovicaleksa234@gmail.com>
2026-01-21 19:54:51 +0100

    Feat: use SecurityContext for auth

--- backend/src/main/java/com/ftn/heisenbugers/gotaxi/controllers/RideController.java
+++ backend/src/main/java/com/ftn/heisenbugers/gotaxi/controllers/RideController.java
@@ -1,12 +1,16 @@
 package com.ftn.heisenbugers.gotaxi.controllers;
 
+import com.ftn.heisenbugers.gotaxi.models.Driver;
+import com.ftn.heisenbugers.gotaxi.models.User;
 import com.ftn.heisenbugers.gotaxi.models.dtos.RideTrackingDTO;
 import com.ftn.heisenbugers.gotaxi.services.RideService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 @RestController
@@ -41,7 +45,12 @@ public class RideController {
 
     @PostMapping("/{rideId}/report")
     public ResponseEntity<Object> reportDriver(@PathVariable UUID rideId, @RequestBody Map<String, Object> body) {
-        boolean ok = rideService.report(rideId, UUID.fromString((String) body.get("reporterId")),
+        User user = (User) Objects.requireNonNull(SecurityContextHolder.getContext().getAuthentication()).getPrincipal();
+        if (user == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        boolean ok = rideService.report(rideId, user.getId(),
                 (String) body.get("description"));
         if (!ok) {
             return ResponseEntity.badRequest().build();
@@ -53,7 +62,12 @@ public class RideController {
 
     @PostMapping("/{rideId}/finish")
     public ResponseEntity<Object> finishRide(@PathVariable UUID rideId) {
-        boolean ok = rideService.finish(rideId);
+        Object sub = Objects.requireNonNull(SecurityContextHolder.getContext().getAuthentication()).getPrincipal();
+        if (!(sub instanceof Driver)) {
+            return ResponseEntity.badRequest().build();
+        }
+        Driver driver = (Driver) sub;
+        boolean ok = rideService.finish(rideId, driver.getId());
         if (!ok) {
             return ResponseEntity.badRequest().build();
         } else {
@@ -66,10 +80,14 @@ public class RideController {
 
     @PostMapping("/{rideId}/rate")
     public ResponseEntity<Object> rateDriver(@PathVariable UUID rideId, @RequestBody Map<String, Object> body) {
+        Object sub = Objects.requireNonNull(SecurityContextHolder.getContext().getAuthentication()).getPrincipal();
+        if (!(sub instanceof User rater)) {
+            return ResponseEntity.badRequest().build();
+        }
         int driverScore = Integer.parseInt((String) body.get("driverScore"));
         int vehicleScore = Integer.parseInt((String) body.get("vehicleScore"));
         String comment = (String) body.get("comment");
-        rideService.rate(rideId, driverScore, vehicleScore, comment);
+        rideService.rate(rideId, rater.getId(), driverScore, vehicleScore, comment);
         return ResponseEntity.ok()
                 .body(Map.of("message", "Ride successfully rated"));
     }
