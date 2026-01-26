8e5236286d772658313c84642e9161f3aabdbad7
vasilicbosko <vasilic.bosko@jjzmaj.edu.rs>
2026-01-26 23:20:08 +0100

    Feat: add getRole functionality

--- web/src/app/components/auth/auth.service.ts
+++ web/src/app/components/auth/auth.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {
   RegisterResponseDTO,
   MessageResponse,
   LoginRequestDTO,
   LoginResponseDTO
 } from './auth.api';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   // without proxy.conf.json
   private baseUrl = 'http://localhost:8081/api/auth';
 
-  constructor(private http: HttpClient) {}
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    skip: 'true',
+  });
+
+  user$ = new BehaviorSubject("");
+  userState = this.user$.asObservable();
+
+  constructor(private http: HttpClient) {
+    this.user$.next(this.getRole());
+  }
 
   register(formData: FormData): Observable<RegisterResponseDTO> {
     //return this.http.post<RegisterResponseDTO>(`${this.baseUrl}/register`, dto);
@@ -30,5 +41,22 @@ export class AuthService {
 
   logout(): Observable<MessageResponse> {
     return this.http.delete<MessageResponse>(`${this.baseUrl}/session`);
+  }
+
+  getRole(): any {
+    if (this.isLoggedIn()) {
+      const accessToken: any = localStorage.getItem('accessToken');
+      const helper = new JwtHelperService();
+      return helper.decodeToken(accessToken).role;
+    }
+    return null;
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem('accessToken') != null;
+  }
+
+  setUser(): void {
+    this.user$.next(this.getRole());
   }
 }
