92214d97e37ff3b445ebbf5776e1efd9a6c9fd74
Aleksa Dejanović <dejanovicaleksa234@gmail.com>
2026-01-28 00:34:12 +0100

    Fix: fix traffic violations and rate modal

--- backend/src/main/java/com/ftn/heisenbugers/gotaxi/repositories/TrafficViolationRepository.java
+++ backend/src/main/java/com/ftn/heisenbugers/gotaxi/repositories/TrafficViolationRepository.java
@@ -1,9 +1,14 @@
 package com.ftn.heisenbugers.gotaxi.repositories;
 
+import com.ftn.heisenbugers.gotaxi.models.Ride;
 import com.ftn.heisenbugers.gotaxi.models.TrafficViolation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TrafficViolationRepository extends JpaRepository<TrafficViolation, UUID> {
+    List<TrafficViolation> getAllById(UUID id);
+
+    List<TrafficViolation> getAllByRide(Ride ride);
 }
