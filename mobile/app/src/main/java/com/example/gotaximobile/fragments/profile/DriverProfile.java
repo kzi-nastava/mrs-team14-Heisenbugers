028b728e0131055dcb2fcdedb7cde8c43827f1dc
vasilicbosko <vasilic.bosko@jjzmaj.edu.rs>
2026-02-07 00:37:17 +0100

    Feat: add driver data loading on profile

--- mobile/app/src/main/java/com/example/gotaximobile/fragments/profile/DriverProfile.java
+++ mobile/app/src/main/java/com/example/gotaximobile/fragments/profile/DriverProfile.java
@@ -2,41 +2,91 @@ package com.example.gotaximobile.fragments.profile;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.gotaximobile.R;
+import com.example.gotaximobile.models.dtos.CreateVehicleDTO;
+import com.example.gotaximobile.models.dtos.GetProfileDTO;
+import com.example.gotaximobile.network.RetrofitClient;
+
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DriverProfile extends Fragment {
+
+    private ProfileCardView activeTodayCard, modelCard, typeCard, plateNoCard, seatsCard, babiesAllowedCard, petsAllowedCard;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_driver_profile, container, false);
 
-        ProfileCardView activeTodayCard = view.findViewById(R.id.card_active_hours);
-        activeTodayCard.setData("Active In Last 24H", "6H", R.drawable.ic_active_time);
+        activeTodayCard = view.findViewById(R.id.card_active_hours);
+        modelCard = view.findViewById(R.id.card_model);
+        typeCard = view.findViewById(R.id.card_type);
+        plateNoCard = view.findViewById(R.id.card_plate_no);
+        seatsCard = view.findViewById(R.id.card_seats);
+        babiesAllowedCard = view.findViewById(R.id.card_babies_allowed);
+        petsAllowedCard = view.findViewById(R.id.card_pets_allowed);
 
-        ProfileCardView modelCard = view.findViewById(R.id.card_model);
-        modelCard.setData("Model", "Ford Fiesta", R.drawable.ic_car);
-
-        ProfileCardView typeCard = view.findViewById(R.id.card_type);
-        typeCard.setData("Type", "Standard", R.drawable.ic_type);
-
-        ProfileCardView plateNoCard = view.findViewById(R.id.card_plate_no);
-        plateNoCard.setData("Plate No.", "NS-254-KL", R.drawable.ic_license_plate);
-
-        ProfileCardView seatsCard = view.findViewById(R.id.card_seats);
-        seatsCard.setData("Seats", "5", R.drawable.ic_car_seat);
-
-        ProfileCardView babiesAllowedCard = view.findViewById(R.id.card_babies_allowed);
-        babiesAllowedCard.setData("Baby Allowed", "Yes", R.drawable.ic_baby);
-
-        ProfileCardView petsAllowedCard = view.findViewById(R.id.card_pets_allowed);
-        petsAllowedCard.setData("Pets Allowed", "No", R.drawable.ic_pets);
+        fetchActiveHours();
+        fetchVehicleInfo();
 
         return view;
+    }
+
+    private void fetchActiveHours(){
+        RetrofitClient.profileService(getContext()).getDriverActiveHours().enqueue(new Callback<Integer>() {
+            @Override
+            public void onResponse(@NonNull Call<Integer> call, @NonNull Response<Integer> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    int time = response.body();
+                    activeTodayCard.setData("Active In Last 24H", String.valueOf(time), R.drawable.ic_active_time);
+                } else {
+                    Log.e("API_ERROR", "Response failed: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Integer> call, @NonNull Throwable t) {
+                Log.e("NETWORK_ERROR", Objects.requireNonNull(t.getMessage()));
+            }
+        });
+    }
+
+    private void fetchVehicleInfo(){
+        RetrofitClient.profileService(getContext()).getDriverVehicle().enqueue(new Callback<CreateVehicleDTO>() {
+            @Override
+            public void onResponse(@NonNull Call<CreateVehicleDTO> call, @NonNull Response<CreateVehicleDTO> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    CreateVehicleDTO vehicle = response.body();
+                    updateView(vehicle);
+                } else {
+                    Log.e("API_ERROR", "Response failed: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<CreateVehicleDTO> call, @NonNull Throwable t) {
+                Log.e("NETWORK_ERROR", Objects.requireNonNull(t.getMessage()));
+            }
+        });
+    }
+
+    private void updateView(CreateVehicleDTO vehicle) {
+        modelCard.setData("Model", vehicle.vehicleModel, R.drawable.ic_car);
+        typeCard.setData("Type", vehicle.vehicleType, R.drawable.ic_type);
+        plateNoCard.setData("Plate No.", vehicle.licensePlate, R.drawable.ic_license_plate);
+        seatsCard.setData("Seats", String.valueOf(vehicle.seatCount), R.drawable.ic_car_seat);
+        babiesAllowedCard.setData("Baby Allowed", vehicle.babyTransport ? "Yes" : "No", R.drawable.ic_baby);
+        petsAllowedCard.setData("Pets Allowed", vehicle.petTransport ? "Yes" : "No", R.drawable.ic_pets);
     }
 }
--- mobile/app/src/main/java/com/example/gotaximobile/network/ProfileService.java
+++ mobile/app/src/main/java/com/example/gotaximobile/network/ProfileService.java
@@ -1,11 +1,24 @@
 package com.example.gotaximobile.network;
 
+import com.example.gotaximobile.models.dtos.ChangePasswordDTO;
+import com.example.gotaximobile.models.dtos.CreateVehicleDTO;
 import com.example.gotaximobile.models.dtos.GetProfileDTO;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
 
 public interface ProfileService {
     @GET("api/profile/me")
     Call<GetProfileDTO> getProfileInfo();
+
+    @GET("api/profile/me/driver")
+    Call<Integer> getDriverActiveHours();
+
+    @GET("api/profile/me/vehicle")
+    Call<CreateVehicleDTO> getDriverVehicle();
+
+    @PUT("api/profile/me/password")
+    Call<Void> changePassword(@Body ChangePasswordDTO body);
 }
