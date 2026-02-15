d20ab4e0059eb87cdc1e3451f2194e5235336e1d
vasilicbosko <vasilic.bosko@jjzmaj.edu.rs>
2026-02-15 14:17:58 +0100

    Feat: add driver registration component test

--- web/src/app/services/chat.service.ts
+++ web/src/app/services/chat.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import * as Stomp from 'stompjs';
+import {Client, Stomp} from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 import { Message } from '../components/chat/chat.component';
 import { HttpClient } from '@angular/common/http';
@@ -21,7 +21,7 @@ export class ChatService {
         }
     });
   }
-  private stompClient!: Stomp.Client;
+  private stompClient!: Client;
   private messagesSubject = new Subject<Message>();
   private chatId: string = '';
 
@@ -32,26 +32,39 @@ export class ChatService {
 
   // Connect to WebSocket
   connect(isAdmin: boolean = false): void {
-    const socket = new SockJS('http://localhost:8081/ws');
-    this.stompClient = Stomp.over(socket);
+    this.stompClient = new Client({
+      webSocketFactory: () => new SockJS('http://localhost:8081/ws'),
+      connectHeaders: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+      },
+      reconnectDelay: 5000,
+      debug: (str) => {
+        console.log(str);
+      }
+    });
 
-    this.stompClient.connect({Authorization: `Bearer ${localStorage.getItem('accessToken')}`}, () => {
+    this.stompClient.onConnect = () => {
       if (isAdmin) {
-        // Admin receives all messages
         this.stompClient.subscribe('/topic/admin', (msg) => {
           this.messagesSubject.next(JSON.parse(msg.body));
         });
       } else {
-        // User receives only their messages
-        this.stompClient.subscribe(`/user/queue/messages`, (msg) => {
+        this.stompClient.subscribe('/user/queue/messages', (msg) => {
           this.messagesSubject.next(JSON.parse(msg.body));
         });
       }
-    });
+    };
+
+    this.stompClient.activate();
+
   }
 
   // Send a message
   sendMessage(message: Message, chatId = this.chatId): void {
-    this.stompClient.send('/app/sendMessage', {}, JSON.stringify({...message, chatId: chatId}));
+    this.stompClient.publish({
+      destination: '/app/sendMessage',
+      body: JSON.stringify({ ...message, chatId })
+    });
   }
+
 }
