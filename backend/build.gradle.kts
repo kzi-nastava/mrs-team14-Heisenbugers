2d5bb6d1a355dab1d87d311b05aa6ff591666655
vasilicbosko <vasilic.bosko@jjzmaj.edu.rs>
2026-02-15 17:17:22 +0100

    Feat: add dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -57,6 +57,15 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Selenium
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.18.1")
+
+    // WebDriverManager
+    testImplementation("io.github.bonigarcia:webdrivermanager:6.1.0")
+
+    // TestNG
+    testImplementation("org.testng:testng:7.12.0")
 }
 
 tasks.withType<Test> {
