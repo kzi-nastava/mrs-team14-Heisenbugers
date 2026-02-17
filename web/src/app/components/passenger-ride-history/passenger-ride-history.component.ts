a695c59d540b549dbdc60cc98596cd09acea0104
Aleksa Dejanović <dejanovicaleksa234@gmail.com>
2026-02-17 04:16:46 +0100

    Ch: move rating to passenger history

--- web/src/app/components/passenger-ride-history/passenger-ride-history.component.ts
+++ web/src/app/components/passenger-ride-history/passenger-ride-history.component.ts
@@ -7,35 +7,45 @@ import {
   bootstrapArrowRight,
   bootstrapClock,
   bootstrapCash,
-  bootstrapExclamationCircleFill
+  bootstrapExclamationCircleFill,
+  bootstrapFeather
 } from '@ng-icons/bootstrap-icons';
 
 import { RideInfo } from '../../models/driver-info.model';
+import { RateModal } from "../rate-modal/rate-modal.component";
+import { RateService } from '../../services/rate.service';
 
 @Component({
   standalone: true,
   selector: 'app-passenger-ride-history',
-  imports: [CommonModule,NgIcon],
+  imports: [CommonModule, NgIcon, RateModal],
   templateUrl: './passenger-ride-history.component.html',
   viewProviders: [provideIcons({
     bootstrapArrowRight,
     bootstrapClock,
     bootstrapCash,
-    bootstrapExclamationCircleFill
+    bootstrapExclamationCircleFill,
+    bootstrapFeather
   })]
 })
 export class PassengerRideHistoryComponent {
+
   private baseUrl = 'http://localhost:8081/api';
 
   rides: RideInfo[] = [];
   loading = false;
+  ratingRide: RideInfo | null = null;
+  
 
   private endpoint = `${this.baseUrl}/users/history`;
+  toastMessage: string = "";
+  toastVisible: boolean = false;
 
   constructor(
     private http: HttpClient,
     private router: Router,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private rateService: RateService
   ) {}
 
   ngOnInit(): void {
@@ -65,4 +75,42 @@ export class PassengerRideHistoryComponent {
   goToRide(ride: RideInfo) {
     this.router.navigate(['/passenger-ride-history', ride.rideId], { state: { ride:ride } });
   }
+
+  openRateModal(ride: RideInfo) {
+    this.ratingRide = ride;
+    console.log(this.ratingRide);
+  }
+
+  closeRateModal() {
+    this.ratingRide = null;
+  }
+  showToast(message: string, duration: number = 2000) {
+    console.log(`Trying to show ${message}`)
+    this.toastMessage = message;
+    this.toastVisible = true;
+    this.cdr.markForCheck();
+    setTimeout(() => {this.toastVisible = false; this.cdr.markForCheck()}, duration);
+  }
+
+  submitRateForm(data: { driverRate: number; vehicleRate: number; comment: string; }) {
+    let sendingData = {
+        "driverScore": data.driverRate,
+        "vehicleScore": data.vehicleRate,
+        "comment": data.comment,
+      }
+      this.rateService.sendRate(this.ratingRide!.rideId, sendingData)
+      .subscribe({
+      next: () => this.showToast('Rating recorded successfully!'),
+      error: (error) => {
+        if (error.status === 409){
+          this.showToast('You have already rated this ride')
+        } else {
+          this.showToast('Failed to record rating')
+        }
+      }
+    })
+    this.closeRateModal();
+    console.log(sendingData)
+  }
+  
 }
