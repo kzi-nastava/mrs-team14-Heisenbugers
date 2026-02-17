cd114b4ede5d91aef73adb13633cb4716de4ef76
vasilicbosko <vasilic.bosko@jjzmaj.edu.rs>
2026-02-17 03:53:35 +0100

    Fix: fix price repository function

--- backend/src/main/java/com/ftn/heisenbugers/gotaxi/repositories/PriceRepository.java
+++ backend/src/main/java/com/ftn/heisenbugers/gotaxi/repositories/PriceRepository.java
@@ -3,9 +3,12 @@ package com.ftn.heisenbugers.gotaxi.repositories;
 import com.ftn.heisenbugers.gotaxi.models.Price;
 import com.ftn.heisenbugers.gotaxi.models.enums.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface PriceRepository extends JpaRepository<Price, UUID> {
-    double getStartingPriceByVehicleType(VehicleType vehicleType);
+    @Query("SELECT p.startingPrice FROM Price p WHERE p.vehicleType = :vehicleType")
+    double getStartingPriceByVehicleType(@Param("vehicleType") VehicleType vehicleType);
 }
